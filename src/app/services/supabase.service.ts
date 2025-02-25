eb71da9c598dea44e84015777534fdb7775ab72b
hosseinkhoshkar <h.khoshkar@gmail.com>
2025-02-26 03:25:42 +0330

    redirectURL

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -3,6 +3,18 @@ import {AuthChangeEvent, AuthSession, createClient, Session, SupabaseClient, Use
 import {environment} from '../../environments/environment';
 import {Profile} from '../interfaces/profile';
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include https:// when not localhost.
+  url = url.startsWith('http') ? url : https://${url}
+  // Make sure to include a trailing /.
+  url = url.endsWith('/') ? url : ${url}/
+  return url
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +29,7 @@ export class SupabaseService {
   signInWithOAuth(provider: 'google' | 'github') {
     return this.supabase.auth.signInWithOAuth({
       provider,
-      options: { redirectTo: `https://deutsche-flash-cards.vercel.app/dashboard` }, // Redirect user after login
+      options: {redirectTo: getURL(), }, // Redirect user after login
     });
   }
 
